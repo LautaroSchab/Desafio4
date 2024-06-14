5ba6c65e083159b9a7aaeb7bac4713a1b2041cfd
Lautaro <Lautaro@DESKTOP-70FPLRL>
2024-06-14 17:52:21 -0300

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿
+using Desafio4.Models;
+
+namespace Desafio4
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<FrutaInformacion> ManzanaInfo { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            ManzanaInfo = new List<FrutaInformacion>()
+            {
+                new FrutaInformacion
+                {
+                    Micronutrientes="Vitamina C",
+                    Porcentaje=8
+                },
+                new FrutaInformacion
+                {
+                    Micronutrientes="Vitamina A",
+                    Porcentaje=1
+                },
+                new FrutaInformacion
+                {
+                    Micronutrientes="Calcio",
+                    Porcentaje=1
+                },
+                new FrutaInformacion
+                {
+                    Micronutrientes="Magnesio",
+                    Porcentaje=1
+                },
+                new FrutaInformacion
+                {
+                    Micronutrientes="potasio",
+                    Porcentaje=3
+                }
+            };
+            this.BindingContext = this;
+        }
+
+       
+    }
+
+}
---
+++ Models/FrutaInformacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio4.Models
+{
+    public class FrutaInformacion
+    {
+        public string Micronutrientes { get; set; }
+        public int Porcentaje { get; set; }
+    }
+}
